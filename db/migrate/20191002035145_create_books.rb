ba84643dd2e3c9e894195ec40b50e43f3fee0ed7
alex <alcebaeza@gmail.com>
2019-10-02 00:59:11 -0300

    se agrego books en vez de portfolio, via resource-generator

---
+++ db/migrate/20191002035145_create_books.rb
@@ -0,0 +1,13 @@
+class CreateBooks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.string :subtitle
+      t.text :body
+      t.text :main_image
+      t.text :thumb_image
+
+      t.timestamps
+    end
+  end
+end
