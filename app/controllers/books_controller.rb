711e7b392c3c1ecef43b2186b19b4135258c1668
alex <alcebaeza@gmail.com>
2019-10-03 15:14:33 -0300

    listo el new y create de book. Falta el show para mostrarlo

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,25 @@ class BooksController < ApplicationController
     def index 
         @book_items = Book.all
     end
+    
+    def new 
+        @book_item = Book.new
+    end
+
+    def create 
+        @book_item = Book.new(book_params)
+
+        respond_to do |format|
+            if @book_item.save 
+                format.html { redirect_to @book_item, notice: 'your book item is now live'}
+            else
+                format.html {render :new}
+            end
+        end
+    end
 end
+
+private 
+    def book_params 
+        params.require(:book).permit(:title, :subtitle, :body)
+    end
