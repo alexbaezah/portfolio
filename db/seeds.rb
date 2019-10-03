19b71f72cef5e4dcde87924e5a76799c52055225
alex <alcebaeza@gmail.com>
2019-10-03 00:51:55 -0300

    creado el seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,52 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |blog| 
+    Blog.create!(
+        title: "my blog post number #{blog}",
+
+        body: 'Lorem Ipsum is simply dummy text of the 
+        printing and typesetting industry. Lorem Ipsum 
+        has been the industry standard dummy text ever since 
+        the 1500s, when an unknown printer took a galley of type 
+        and scrambled it to make a type specimen book. It has survived 
+        not only five centuries, but also the leap into electronic typesetting, 
+        remaining essentially unchanged. It was popularised in the 1960s with
+         the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+        
+        
+    )
+end
+
+puts '10 blogs created'
+
+5.times do |book|
+    Book.create!(
+        title: "my book post number #{book}",
+        subtitle: "my subtitle number #{book}",
+        body: 'Lorem Ipsum is simply dummy text of the 
+        printing and typesetting industry. Lorem Ipsum 
+        has been the industry standard dummy text ever since 
+        the 1500s, when an unknown printer took a galley of type 
+        and scrambled it to make a type specimen book. It has survived 
+        not only five centuries, but also the leap into electronic typesetting, 
+        remaining essentially unchanged. It was popularised in the 1960s with
+        the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+        
+        main_image: "https://via.placeholder.com/350x150",
+
+        thumb_image: "https://via.placeholder.com/150"
+    )
+end
+
+puts '5 books created'
+
+9.times do |skill| 
+    Skill.create!(
+       title: "rails #{skill}",
+        percent_utilized: 15,
+    )
+end
+
+puts '9 skills created'
